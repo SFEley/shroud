57313a938f447e604708acde96277efff47e11d0
Steve Eley <sfeley@gmail.com>
2012-12-25 14:09:00 -0600

    Initial Rails 4 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shroud::Application.config.secret_key_base = 'd12442236e920d0957ddf52adf92de3bb77106e7ceb782c4730ba773c646d6022924c3be42bfc022e6b7913b22691785fb374efc45ae67a3075fc4737c810ad8'
